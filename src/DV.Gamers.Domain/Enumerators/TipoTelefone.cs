5bb22f4fa86d8b885a101e8c79466c187467fa26
Diego Augusto <diego.bitsoftware@gmail.com>
2020-10-03 19:11:50 -0300

    Inclusão tipo telefone

---
+++ src/DV.Gamers.Domain/Enumerators/TipoTelefone.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DV.Gamers.Domain.Enumerators
+{
+    public enum TipoTelefone
+    {
+        Residencial,
+        Celular,
+        Comercial
+    }
+}
